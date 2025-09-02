8a89be7652645dd3a2399723842e5a2f72c4059d
lucastreille <lucas.treille1.0@gmail.com>
2025-09-02 16:08:55 +0200

    feat(app): add standalone config and lazy routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,14 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimations(),
+    provideHttpClient(),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
+  },
+  {
+    path: 'etudiants',
+    loadChildren: () =>
+      import('./features/etudiants/etudiants.routes').then((m) => m.ETUDIANTS_ROUTES),
+  },
+  {
+    path: 'cours',
+    loadChildren: () => import('./features/cours/cours.routes').then((m) => m.COURS_ROUTES),
+  },
+  {
+    path: 'notes',
+    loadChildren: () => import('./features/notes/notes.routes').then((m) => m.NOTES_ROUTES),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./shared/pages/not-found.page').then((m) => m.NotFoundPage),
+  },
+];
